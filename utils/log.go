3d8ed35e9c834ff8292a37cfddb59743ba12d990
fxckcode <diegoale8582@hotmail.com>
2025-01-12 22:05:44 -0500

    feat: add basic client gemini

---
+++ utils/log.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	Log = logrus.New()
+)
+
+func init() {
+	Log.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+		ForceColors:   true,
+	})
+	Log.SetOutput(os.Stdout)
+	Log.SetLevel(logrus.InfoLevel)
+}
